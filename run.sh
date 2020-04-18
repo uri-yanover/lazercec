c5feb95d12a666053c34ba607a226278c5333ed5
Uri Yanover <uri.yanover@gmail.com>
2020-04-18 20:20:27 +0000

    Mark source inactive

--- run.sh
+++ run.sh
@@ -6,4 +6,9 @@ OWN_PATH=$(dirname "$0")
 
 source "${OWN_PATH}/ve/bin/activate"
 
-python3 "${OWN_PATH}/main.py" --configuration "${OWN_PATH}/config.json"
+date 2>&1
+date 1>&2
+# TODO: move into the Python
+echo 'is' | cec-client -s
+python3 "${OWN_PATH}/main.py" -v DEBUG --configuration "${OWN_PATH}/config.json"
+exit $?
