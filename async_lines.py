9a776ed7efa4f27689829a746b43eddf877481dc
Uri Yanover (River Pi) <uri.yanover+pi=river@gmail.com>
2024-01-13 16:10:05 +0200

    Black and send OSD string

--- async_lines.py
+++ async_lines.py
@@ -8,17 +8,22 @@ _LOGGER = getLogger(__name__)
 
 
 @dataclass
-class SubprocessLineDialogue():
+class SubprocessLineDialogue:
     _proc: asyncio.subprocess.Process
     _termination_ordered: bool = False
 
-    async def conduct(self, send_lines: Sequence[str] = (), order_termination: bool=False, timeout_seconds = 5.0):
+    async def conduct(
+        self,
+        send_lines: Sequence[str] = (),
+        order_termination: bool = False,
+        timeout_seconds=5.0,
+    ):
         start_time = monotonic()
         # One way
         self._termination_ordered = self._termination_ordered or order_termination
 
         for line in send_lines:
-            self._proc.stdin.write(line.encode('utf-8') + b'\n')
+            self._proc.stdin.write(line.encode("utf-8") + b"\n")
         await self._proc.stdin.drain()
 
         while True:
@@ -27,53 +32,52 @@ class SubprocessLineDialogue():
                 remaining_time = start_time + timeout_seconds - monotonic()
                 if remaining_time < 0:
                     break
-                output = await asyncio.wait_for(self._proc.stdout.readline(), timeout=remaining_time)
+                output = await asyncio.wait_for(
+                    self._proc.stdout.readline(), timeout=remaining_time
+                )
             except TimeoutError:
                 pass
-                
+
             if self._termination_ordered:
                 self._proc.terminate()
-    
+
             if output is not None:
-                line = output.decode('utf-8')
-                if len(line) > 0 and line[-1] == '\n':
+                line = output.decode("utf-8")
+                if len(line) > 0 and line[-1] == "\n":
                     line = line[:-1]
-                _LOGGER.debug('line %s', line)
+                _LOGGER.debug("line %s", line)
                 yield line
-    
+
         try:
             await asyncio.wait_for(self._proc.wait(), timeout=0.1)
         except TimeoutError:
             pass
-    
-        
+
+
 async def start_dialogue(cmd: Tuple[str, ...]):
-    _LOGGER.info('Starting dialogue, command is %s', cmd)
+    _LOGGER.info("Starting dialogue, command is %s", cmd)
     process = await asyncio.create_subprocess_shell(
         cmd,
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
     )
-        # stderr=asyncio.subprocess.PIPE)
+    # stderr=asyncio.subprocess.PIPE)
     return SubprocessLineDialogue(process)
 
 
 async def _test_start_dialogue():
-    x = await start_dialogue('/bin/cat')
-    
-    x.send_line('hello')
-    x.send_line('hello1')
-    x.send_line('hello2')
-    x.send_line('hello3')
+    x = await start_dialogue("/bin/cat")
+
+    x.send_line("hello")
+    x.send_line("hello1")
+    x.send_line("hello2")
+    x.send_line("hello3")
 
     async for element in x.conduct_dialogue():
         print(element)
-        if '3' in element:
+        if "3" in element:
             x.send_line(None)
 
-    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(_test_start_dialogue())
-
-
--- main.py
+++ main.py
@@ -14,38 +14,46 @@ _LOGGER = logging.getLogger(__name__)
 
 async def get_tv_status(dialogue):
     result = None
-    async for line in dialogue.conduct(('pow 0',)):
-        split_line = line.split('power status: ', 1)
+    async for line in dialogue.conduct(("pow 0",)):
+        split_line = line.split("power status: ", 1)
         if len(split_line) == 2:
-            _LOGGER.debug('reported status %s', split_line[1])
+            _LOGGER.debug("reported status %s", split_line[1])
             result = split_line[1]
 
     return result
     # Can return: 'on', 'standby', 'in transition from standby to on', 'in transition from on to standby'
 
+
 async def set_tv_on(dialogue):
-    async for _ in dialogue.conduct(('on 0',)):
+    async for _ in dialogue.conduct(
+        (
+            "tx 10:" + ":".join(f"{b:02x}" for b in b"Raspberry"),
+            "on 0",
+        )
+    ):
         pass
 
     await asyncio.sleep(15)
     status = await get_tv_status(dialogue)
-    if status != 'on':
-        raise RuntimeError(f'TV not on, status={status}')
-    async for line in dialogue.conduct(('tx 1F:82:20:00',)):  # Switch input source
-        if line == 'waiting for input':
+    if status != "on":
+        raise RuntimeError(f"TV not on, status={status}")
+    async for line in dialogue.conduct(("tx 1F:82:20:00",)):  # Switch input source
+        if line == "waiting for input":
             return
 
+
 async def set_tv_standby(dialogue):
-    async for _ in dialogue.conduct(('standby 0',)):
+    async for _ in dialogue.conduct(("standby 0",)):
         pass
     await get_tv_status(dialogue)
     await asyncio.sleep(15)
     status = await get_tv_status(dialogue)
-    if status != 'standby':
-        raise RuntimeError(f'TV not off, status={status}')
- 
+    if status != "standby":
+        raise RuntimeError(f"TV not off, status={status}")
+
+
 async def main_loop(configuration_file_name):
-    _LOGGER.info('Starting execution loop')
+    _LOGGER.info("Starting execution loop")
     counter = 0
     dialogue = None
 
@@ -53,23 +61,24 @@ async def main_loop(configuration_file_name):
         await asyncio.sleep(1)
         tv_status = None if dialogue is None else await get_tv_status(dialogue)
         if tv_status is None:
-            dialogue = await start_dialogue('/usr/bin/cec-client')
+            dialogue = await start_dialogue("/usr/bin/cec-client")
 
-        _LOGGER.info('Initial TV status %s', tv_status)
+        _LOGGER.info("Initial TV status %s", tv_status)
 
         is_source_up = get_source_status(configuration_file_name)
 
         if counter % 100 == 0:
-            _LOGGER.info('Source up=%s / TV status=%s', is_source_up, tv_status)
+            _LOGGER.info("Source up=%s / TV status=%s", is_source_up, tv_status)
             counter = 0
         counter += 1
 
-        if (is_source_up, tv_status) == (True, 'standby'):
-            _LOGGER.info('Source up, TV standby. Turning TV on.')
+        if (is_source_up, tv_status) == (True, "standby"):
+            _LOGGER.info("Source up, TV standby. Turning TV on.")
             await set_tv_on(dialogue)
-        elif (is_source_up, tv_status) == (False, 'on'):
-            _LOGGER.info('Source down, TV on. Setting TV to standby.')
+        elif (is_source_up, tv_status) == (False, "on"):
+            _LOGGER.info("Source down, TV on. Setting TV to standby.")
             await set_tv_standby(dialogue)
+
 
 @dataclass
 class _Configuration(object):
@@ -78,11 +87,13 @@ class _Configuration(object):
 
 
 def get_source_status(configuration_file_name: str):
-    with open(configuration_file_name, 'rt') as configuration_file:
+    with open(configuration_file_name, "rt") as configuration_file:
         configuration = _Configuration(**load(configuration_file))
 
-        outcome = subprocess.run(configuration.poll_shell_script, shell=True, capture_output=True)
-        _LOGGER.debug(f'Check outcome is {outcome}')
+        outcome = subprocess.run(
+            configuration.poll_shell_script, shell=True, capture_output=True
+        )
+        _LOGGER.debug(f"Check outcome is {outcome}")
 
         return outcome.returncode == 0
 
@@ -92,11 +103,26 @@ def _logging_levelstr_to_level(levelstr: str):
 
 
 @click.command()
-@click.option('-v', '--logging-level', type=_logging_levelstr_to_level, help='Logging level', default='INFO')
-@click.option('-c', '--configuration', 'configuration_file_name', type=click.Path(), help='JSON configuration file')
+@click.option(
+    "-v",
+    "--logging-level",
+    type=_logging_levelstr_to_level,
+    help="Logging level",
+    default="INFO",
+)
+@click.option(
+    "-c",
+    "--configuration",
+    "configuration_file_name",
+    type=click.Path(),
+    help="JSON configuration file",
+)
 def main(configuration_file_name: str, logging_level):
-    logging.basicConfig(level=logging_level, format='%(asctime)s %(levelname)-8s %(message)s')
+    logging.basicConfig(
+        level=logging_level, format="%(asctime)s %(levelname)-8s %(message)s"
+    )
     asyncio.get_event_loop().run_until_complete(main_loop(configuration_file_name))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
