89e77d2a92c06730df7be2103899b9aae258d11f
Uri Yanover Yahav <uri.yanover+github+commit@gmail.com>
2025-06-01 14:58:40 +0300

    Fix comments

--- orchestration/run_until.sh
+++ orchestration/run_until.sh
@@ -6,11 +6,11 @@ shift
 # Get the current time in seconds since the epoch
 CURRENT_TS=$(date +%s)
 
-# Get the timestamp for 3 AM tomorrow in seconds since the epoch
+# Get the timestamp for the goal time in seconds since the epoch
 END_TS=$(date -d "${RUN_UNTIL}" +%s)
 
-# Calculate the timeout duration in seconds
+# Calculate the timeout duration in seconds, plus a deliberate margin
 timeout_seconds=$((5 + END_TS - CURRENT_TS))
 
-# Run the process with timeout
+# Run the proper arguments
 timeout "${timeout_seconds}" "${@}"
